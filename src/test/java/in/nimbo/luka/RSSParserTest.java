9f98be7f19cc45fe80fc4208538443693d8059b6
Sina Nadi <s.nadi1376@gmail.com>
2018-07-17 15:10:51 +0430

    Unit test for RssParser

---
+++ src/test/java/in/nimbo/luka/RSSParserTest.java
@@ -0,0 +1,23 @@
+package in.nimbo.luka;
+
+import in.nimbo.luka.feed.rss.Channel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+
+public class RSSParserTest {
+    @Test
+    public void testRSSParserWithTabnakSite () throws IOException {
+        RSSParser rssParser = new RSSParser();
+        Properties properties = new Properties();
+        properties.load(this.getClass().getClassLoader().getResourceAsStream(""));
+        URL url = new URL(properties.get("TABNAK_NEWS_URL").toString());
+        Config config = new Config(".gutter_news > div.body",null);
+        Channel expected = new Channel();
+        Channel output = rssParser.parse(url,config);
+        assertEquals (expected,output);
+    }
+}
