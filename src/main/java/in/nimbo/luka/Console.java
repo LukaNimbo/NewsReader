4f3c439b38738a1823c1660604d1d6f0636ea4fa
Reza Bayat <reza.bayat76@gmail.com>
2018-07-17 23:16:55 +0430

    [MASTER <CREATE FILE>] -> create Console class (not implemented)

--- src/main/java/in/nimbo/luka/Console.java
+++ src/main/java/in/nimbo/luka/Console.java
@@ -1,4 +1,14 @@
 package in.nimbo.luka;
 
+import asg.cliche.Command;
+import asg.cliche.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Console {
+    private final static Logger logger = LoggerFactory.getLogger(Console.class);
+    public Console(){
+
+    }
 }
+
