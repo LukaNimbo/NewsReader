e9e2fab20da0fdfbd479bc9b96130434a68388f3
Sina Nadi <s.nadi1376@gmail.com>
2018-07-17 15:14:35 +0430

    write unit test for HTMLParser

--- src/test/java/in/nimbo/luka/HTMLParserTest.java
+++ src/test/java/in/nimbo/luka/HTMLParserTest.java
@@ -1,7 +1,22 @@
 package in.nimbo.luka;
 
+import org.junit.Test;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
 import static org.junit.Assert.*;
 
-public class XMLParserTest {
-
+public class HTMLParserTest {
+    @Test
+    public void testHTMLParserWithTabnakSite () throws IOException {
+        HTMLParser htmlParse = new HTMLParser();
+        Properties properties = new Properties();
+        properties.load(this.getClass().getClassLoader().getResourceAsStream("./HTMLParserTest.txt"));
+        URL url = new URL(properties.get("TABNAK_NEWS_URL").toString());
+        Config config = new Config(".gutter_news > div.body",null);
+        String context = htmlParse.parse(url,config);
+        System.out.println(properties.get("TABNAK_NEWS_CONTEXT").toString());
+        //assertEquals(properties.get("TABNAK_NEWS_CONTEXT").toString(), context);
+        assertTrue(context.contains("مذاکره با آمریکا"));
+    }
 }
