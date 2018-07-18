bc3128a847df85357f12e0e072db1aad0ec5cffc
Reza Bayat <reza.bayat76@gmail.com>
2018-07-18 15:12:24 +0430

    [MASTER <UPDATE FILE>] -> setup of HikariConnetionPool extract from dbConfig file

--- src/main/java/in/nimbo/luka/database/HikariConnectionPool.java
+++ src/main/java/in/nimbo/luka/database/HikariConnectionPool.java
@@ -12,12 +12,8 @@ import java.sql.SQLException;
 import java.util.Properties;
 
 public class HikariConnectionPool {
-
-    private final String DATABASE_JDBC_URL = "jdbc:mysql://localhost/NewsReader";
-    private final String DATABASE_USERNAME = "reza";
-    private final String DATABASE_PASSWORD = "root";
     private volatile HikariDataSource hikariDataSource;
-
+    private final String DATABASE_DATABASE_CONFIG_FILE_PATH = "src/main/resources/dbConfig.properties";
 
     private HikariConnectionPool(){
         setup();
@@ -33,17 +29,23 @@ public class HikariConnectionPool {
 
     private void setup(){
 
-//        Properties dbProperties = getDBProperties();
-//        if (dbProperties.isEmpty()){
-//            //TODO: dpProperties is empty, must be log.
-//        }
-        System.out.println("[SETUP HikariConnectionPool]");
+        Properties dbProperties = getDBProperties();
+        if (dbProperties.isEmpty()){
+            //TODO: dpProperties is empty, must be log.
+        }
+
+        String jdbcURLTemplate = dbProperties.getProperty("jdbcURLTemplate");
+        String dbUsername = dbProperties.getProperty("username");
+        String dbPassword= dbProperties.getProperty("password");
+        String dbHostname = dbProperties.getProperty("hostname");
+        String dbDatabaseName= dbProperties.getProperty("databaseName");
+        String jdbcURL = String.format(jdbcURLTemplate, dbHostname, dbDatabaseName);
 
         HikariConfig config = new HikariConfig();
-        config.setJdbcUrl(DATABASE_JDBC_URL);
-        config.setUsername(DATABASE_USERNAME);
-        config.setPassword(DATABASE_PASSWORD);
 
+        config.setJdbcUrl(jdbcURL);
+        config.setUsername(dbUsername);
+        config.setPassword(dbPassword);
         config.addDataSourceProperty("cachePrepStmts", "true");
         config.addDataSourceProperty("prepStmtCacheSize", "250");
         config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
@@ -56,7 +58,7 @@ public class HikariConnectionPool {
         Properties dbProperties = new Properties();
         InputStream inputStream = null;
         try {
-            inputStream = new FileInputStream("/dbConfig.properties");
+            inputStream = new FileInputStream(DATABASE_DATABASE_CONFIG_FILE_PATH);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
