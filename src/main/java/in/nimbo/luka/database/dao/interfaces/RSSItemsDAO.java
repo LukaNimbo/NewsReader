bb30d088c6b09d1cfd21236e51b8eece69ca6a1a
Reza Bayat <reza.bayat76@gmail.com>
2018-07-19 10:31:47 +0430

    [MASTER <UPDATE FILE>] -> delete methods

--- src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
@@ -1,18 +1,5 @@
 package in.nimbo.luka.database.dao.interfaces;
 
-import in.nimbo.luka.feed.rss.Item;
-
-import java.sql.SQLException;
-import java.util.Date;
-import java.util.List;
-
 public interface RSSItemsDAO {
 
-    boolean existsItem(Item item) throws SQLException;
-
-    void addItem(Item item) throws SQLException;
-
-    List<Item> getLastNewsOfChannel(int numOfRows, int channelId) throws SQLException;
-
-    int getNumberOfItemsInChannelPerDay(Date dayDate, int channelId) throws SQLException;
 }
