de813e6447778c2ed1d884da8fb3a6a0c03a67e3
Reza Bayat <reza.bayat76@gmail.com>
2018-07-19 10:30:59 +0430

    [MASTER <UPDATE FILE>] -> delete unused imports

--- src/main/java/in/nimbo/luka/Console.java
+++ src/main/java/in/nimbo/luka/Console.java
@@ -1,13 +1,12 @@
 package in.nimbo.luka;
 
-import asg.cliche.Command;
-import asg.cliche.Param;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Console {
     private final static Logger logger = LoggerFactory.getLogger(Console.class);
     public Console(){
+
     }
 }
 
