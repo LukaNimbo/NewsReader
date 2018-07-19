e11e2bd038bacc41e05e25b03ecfb2dcf439c7cd
Reza Bayat <reza.bayat76@gmail.com>
2018-07-19 12:13:39 +0430

    [MASTER <UPDATE FILE>] -> create interface of SiteChannelDAO

--- src/main/java/in/nimbo/luka/database/dao/interfaces/SiteChannelDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/SiteChannelDAO.java
@@ -1,4 +1,20 @@
 package in.nimbo.luka.database.dao.interfaces;
 
+import in.nimbo.luka.feed.rss.Channel;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface SiteChannelDAO {
+
+    boolean existChannel(int channelId) throws SQLException;
+
+    void addChannel(Channel channel) throws SQLException;
+
+    Channel getChannel(int channelId) throws SQLException;
+
+    int getChannelId(String rssLink) throws SQLException;
+
+    List<Channel> getAllChannels() throws SQLException;
+
 }
