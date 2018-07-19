58a663b7d0697ebadf584cf2b5d7a71c4c8e3962
Sina Nadi <s.nadi1376@gmail.com>
2018-07-19 11:49:30 +0430

    [App.java] --> Add ConsoleShell

--- src/main/java/in/nimbo/luka/App.java
+++ src/main/java/in/nimbo/luka/App.java
@@ -1,9 +1,13 @@
 package in.nimbo.luka;
 
+import asg.cliche.*;
+
+import java.io.IOException;
 
 public class App {
-    public static void main( String[] args ) {
+    public static void main( String[] args ) throws IOException {
         NewsReader newsReader = new NewsReader();
         newsReader.initialize();
+        ShellFactory.createConsoleShell("Hello","NewsReader",new Console()).commandLoop();
     }
 }
