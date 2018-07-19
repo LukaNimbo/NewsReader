28f0f63b6ab941015ae36881bf1dac0a3a3f80f8
Sina Nadi <s.nadi1376@gmail.com>
2018-07-19 11:52:32 +0430

    [SiteConfigDAO.java] --> remove unused imports

--- src/main/java/in/nimbo/luka/database/dao/interfaces/SiteConfigDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/SiteConfigDAO.java
@@ -4,12 +4,11 @@ import in.nimbo.luka.SiteConfig;
 
 import java.sql.SQLException;
 
+
 public interface SiteConfigDAO {
 
     void addConfig(SiteConfig siteConfig) throws SQLException;
-
     SiteConfig getConfig(String link) throws SQLException;
-
     void updateConfig(SiteConfig siteConfig) throws SQLException;
 
 }
