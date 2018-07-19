216378d1bd08f7c6023c678a9da0e7ddd186687b
Reza Bayat <reza.bayat76@gmail.com>
2018-07-19 22:15:41 +0430

    [MASTER <CREATE FILE>] -> define RSSItemsDAO interface

--- src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
@@ -1,5 +1,22 @@
 package in.nimbo.luka.database.dao.interfaces;
 
+import in.nimbo.luka.feed.rss.Item;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
+
 public interface RSSItemsDAO {
+
+    boolean exitsItemById(int itemId) throws SQLException;
+
+    boolean exitsItemByLink(String link) throws SQLException;
+
+    void addItem(Item item) throws SQLException;
+
+    Item getItemById(int itemId) throws SQLException;
+
+    List<Item> getLatestNews(int quantity, int channelId, Date date) throws SQLException;
+
 
 }
