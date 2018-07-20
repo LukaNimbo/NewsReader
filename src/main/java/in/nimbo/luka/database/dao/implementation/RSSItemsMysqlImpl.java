2c4f3e3bca61fe6af3c22b16d68eac0a794c315a
Reza Bayat <reza.bayat76@gmail.com>
2018-07-20 23:35:21 +0430

    [MASTER <UPDATE FILE>] -> add getItems, getNews, getNewsInDay, SearchInContext, SearchInTitle methods

--- src/main/java/in/nimbo/luka/database/dao/implementation/RSSItemsMysqlImpl.java
+++ src/main/java/in/nimbo/luka/database/dao/implementation/RSSItemsMysqlImpl.java
@@ -6,11 +6,11 @@ import in.nimbo.luka.feed.rss.Item;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
 public class RSSItemsMysqlImpl implements RSSItemsDAO {
-
 
     @Override
     public boolean exitsItemById(int itemId) throws SQLException {
@@ -67,19 +67,12 @@ public class RSSItemsMysqlImpl implements RSSItemsDAO {
     }
 
     @Override
-    public List<Item> getNews(int quantity, int channelId, Date date) throws SQLException {
-
+    public List<Item> getItems() throws SQLException {
         List<Item> items = new ArrayList<>();
         Connection connection = HikariConnectionPool.getInstance().getConnection();
-        String getLatestNewsQuery = "SELECT rss_items.id, rss_items.title, rss_items.link, rss_items.pubDate, rss_items.description, rss_items.context, rss_items.site_channel_id " +
-                "FROM site_channel, rss_items " +
-                "WHERE site_channel.id = rss_items.id and rss_items.id = ? and rss_items.pubDate = ?;";
-        PreparedStatement preparedStatement = connection.prepareStatement(getLatestNewsQuery);
-
-        ;
+        String searchInTitleQuery =  "SELECT * FROM rss_items;";
+        PreparedStatement preparedStatement = connection.prepareStatement(searchInTitleQuery);
         ResultSet resultSet = preparedStatement.executeQuery();
-        preparedStatement.setInt(1, channelId);
-        preparedStatement.setTimestamp(2, new Timestamp(date.getTime()));
 
         while (resultSet.next()){
             Item item = new Item();
@@ -92,6 +85,194 @@ public class RSSItemsMysqlImpl implements RSSItemsDAO {
             item.setChannelId(resultSet.getInt("site_channel_id"));
             items.add(item);
         }
+
         return items;
     }
+
+    @Override
+    public List<Item> getNews(int quantity, int channelId, Date date) throws SQLException {
+
+
+        //TODO: inja bayad megah konam
+        List<Item> items = new ArrayList<>();
+        Connection connection = HikariConnectionPool.getInstance().getConnection();
+        String getLatestNewsQuery = "SELECT rss_items.id, rss_items.title, rss_items.link, rss_items.pubDate, rss_items.description, rss_items.context, rss_items.site_channel_id " +
+                "FROM site_channel, rss_items " +
+                "WHERE site_channel.id = rss_items.site_channel_id and site_channel.id = ? and rss_items.pubDate = ? " +
+                "LIMIT ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(getLatestNewsQuery);
+
+        ;
+        ResultSet resultSet = preparedStatement.executeQuery();
+        preparedStatement.setInt(1, channelId);
+        preparedStatement.setTimestamp(2, new Timestamp(date.getTime()));
+        preparedStatement.setInt(3, quantity);
+
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setId(resultSet.getInt("id"));
+            item.setTitle(resultSet.getString("title"));
+            item.setLink(resultSet.getString("link"));
+            item.setPubDate(resultSet.getTimestamp("pubDate"));
+            item.setDescription(resultSet.getString("description"));
+            item.setContext(resultSet.getString("context"));
+            item.setChannelId(resultSet.getInt("site_channel_id"));
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    @Override
+    public List<Item> getNews(int quantity, int channelId) throws SQLException {
+
+        //TODO: inja bayad megah konam
+        List<Item> items = new ArrayList<>();
+        Connection connection = HikariConnectionPool.getInstance().getConnection();
+        String getLatestNewsQuery = "SELECT rss_items.id, rss_items.title, rss_items.link, rss_items.pubDate, rss_items.description, rss_items.context, rss_items.site_channel_id " +
+                "FROM site_channel, rss_items " +
+                "WHERE site_channel.id = rss_items.site_channel_id and site_channel.id = ? " +
+                "ORDER BY rss_items.pubDate LIMIT ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(getLatestNewsQuery);
+
+        ;
+        ResultSet resultSet = preparedStatement.executeQuery();
+        preparedStatement.setInt(1, channelId);
+        preparedStatement.setInt(2, quantity);
+
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setId(resultSet.getInt("id"));
+            item.setTitle(resultSet.getString("title"));
+            item.setLink(resultSet.getString("link"));
+            item.setPubDate(resultSet.getTimestamp("pubDate"));
+            item.setDescription(resultSet.getString("description"));
+            item.setContext(resultSet.getString("context"));
+            item.setChannelId(resultSet.getInt("site_channel_id"));
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    @Override
+    public List<Item> getNewsInDay(int channelId, Date date) throws SQLException {
+        List<Item> items = new ArrayList<>();
+        Connection connection = HikariConnectionPool.getInstance().getConnection();
+        String getNewsInDayQuery =  "SELECT * FROM rss_items WHERE site_channel_id = ? AND pubDate BETWEEN ? AND ?;";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(getNewsInDayQuery);
+        Date startDate = startDate(date);
+        Date endDate = endDate(date);
+        preparedStatement.setInt(1, channelId);
+        preparedStatement.setTimestamp(2, new Timestamp(startDate.getTime()));
+        preparedStatement.setTimestamp(3, new Timestamp(endDate.getTime()));
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setId(resultSet.getInt("id"));
+            item.setTitle(resultSet.getString("title"));
+            item.setLink(resultSet.getString("link"));
+            item.setPubDate(resultSet.getTimestamp("pubDate"));
+            item.setDescription(resultSet.getString("description"));
+            item.setContext(resultSet.getString("context"));
+            item.setChannelId(resultSet.getInt("site_channel_id"));
+            items.add(item);
+        }
+
+        return items;
+    }
+
+    @Override
+    public List<Item> searchInContext(String phrase) throws SQLException {
+        List<Item> items = new ArrayList<>();
+        Connection connection = HikariConnectionPool.getInstance().getConnection();
+        String searchInTitleQuery =  "SELECT * FROM rss_items WHERE context LIKE ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(searchInTitleQuery);
+        preparedStatement.setString(1, "'%" + phrase + "%'");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setId(resultSet.getInt("id"));
+            item.setTitle(resultSet.getString("title"));
+            item.setLink(resultSet.getString("link"));
+            item.setPubDate(resultSet.getTimestamp("pubDate"));
+            item.setDescription(resultSet.getString("description"));
+            item.setContext(resultSet.getString("context"));
+            item.setChannelId(resultSet.getInt("site_channel_id"));
+            items.add(item);
+        }
+
+        return items;
+
+    }
+
+    @Override
+    public List<Item> searchInTitle(String phrase) throws SQLException {
+
+        List<Item> items = new ArrayList<>();
+        Connection connection = HikariConnectionPool.getInstance().getConnection();
+        String searchInTitleQuery =  "SELECT * FROM rss_items WHERE title LIKE ?;";
+        PreparedStatement preparedStatement = connection.prepareStatement(searchInTitleQuery);
+        preparedStatement.setString(1, "'%" + phrase + "%'");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            Item item = new Item();
+            item.setId(resultSet.getInt("id"));
+            item.setTitle(resultSet.getString("title"));
+            item.setLink(resultSet.getString("link"));
+            item.setPubDate(resultSet.getTimestamp("pubDate"));
+            item.setDescription(resultSet.getString("description"));
+            item.setContext(resultSet.getString("context"));
+            item.setChannelId(resultSet.getInt("site_channel_id"));
+            items.add(item);
+        }
+
+        return items;
+
+    }
+//    SELECT *
+//    FROM site_config, site_channel, rss_items
+//    WHERE site_channel.id = site_channel.site_config_id and site_channel.id = rss_items.site_channel_id
+//    ORDER BY rss_items.pubDate DESC LIMIT 10;
+//
+//    List<String> latestNewsOf = new ArrayList<>();
+//    Connection connection = HikariConnectionPool.getInstance().getConnection();
+//    String searchInTitleQuery =  "SELECT *\n" +
+//            "FROM site_config, site_channel, rss_items\n" +
+//            "WHERE site_channel.id = site_channel.site_config_id and site_channel.id = rss_items.site_channel_id\n" +
+//            "ORDER BY rss_items.pubDate DESC LIMIT 10;";
+//    PreparedStatement preparedStatement = connection.prepareStatement(searchInTitleQuery);
+//        preparedStatement.setString(1, "'%" + phrase + "%'");
+//    ResultSet resultSet = preparedStatement.executeQuery();
+    @Override
+    public List<String> getLatestNewsOfAgencies() throws SQLException {
+        return null;
+
+    }
+
+    private Date startDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 23);
+        calendar.set(Calendar.MINUTE, 59);
+        calendar.set(Calendar.SECOND, 59);
+        calendar.set(Calendar.MILLISECOND, 999);
+        return calendar.getTime();
+    }
+
+    private Date endDate(Date date) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar.getTime();
+    }
+
 }
