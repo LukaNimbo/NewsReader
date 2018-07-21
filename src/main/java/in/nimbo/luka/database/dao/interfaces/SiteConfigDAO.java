be5d1f4c5902e9621d75986291eeb063ed612e76
Reza Bayat <reza.bayat76@gmail.com>
2018-07-21 11:31:22 +0430

    [MASTER <UPDATE FILE>] -> change SQLException to Exception

--- src/main/java/in/nimbo/luka/database/dao/interfaces/SiteConfigDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/SiteConfigDAO.java
@@ -1,17 +1,15 @@
 package in.nimbo.luka.database.dao.interfaces;
 
 import in.nimbo.luka.SiteConfig;
-
-import java.sql.SQLException;
 import java.util.List;
 
 
 public interface SiteConfigDAO {
 
-    void addConfig(SiteConfig siteConfig) throws SQLException;
-    SiteConfig getConfig(String link) throws SQLException;
-    void updateConfig(SiteConfig siteConfig) throws SQLException;
+    void addConfig(SiteConfig siteConfig) throws Exception;
+    SiteConfig getConfig(String link) throws Exception;
+    void updateConfig(SiteConfig siteConfig) throws Exception;
 
-    List<SiteConfig> getSiteConfigs() throws SQLException;
+    List<SiteConfig> getSiteConfigs() throws Exception;
 
 }
