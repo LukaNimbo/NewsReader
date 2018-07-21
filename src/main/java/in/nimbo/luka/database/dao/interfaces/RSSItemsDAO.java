5b706f108353d66c92cbd945f156285ed606281d
Reza Bayat <reza.bayat76@gmail.com>
2018-07-21 11:31:07 +0430

    [MASTER <UPDATE FILE>] -> change SQLException to Exception

--- src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
+++ src/main/java/in/nimbo/luka/database/dao/interfaces/RSSItemsDAO.java
@@ -2,33 +2,32 @@ package in.nimbo.luka.database.dao.interfaces;
 
 import in.nimbo.luka.feed.rss.Item;
 
-import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
 
 public interface RSSItemsDAO {
 
-    boolean exitsItemById(int itemId) throws SQLException;
+    boolean exitsItemById(int itemId) throws Exception;
 
-    boolean exitsItemByLink(String link) throws SQLException;
+    boolean exitsItemByLink(String link) throws Exception;
 
-    void addItem(Item item) throws SQLException;
+    void addItem(Item item) throws Exception;
 
-    Item getItemById(int itemId) throws SQLException;
+    Item getItemById(int itemId) throws Exception;
 
-    List<Item> getItems() throws SQLException;
+    List<Item> getItems() throws Exception;
 
-    List<Item> getNews(int quantity, int channelId) throws SQLException;
+    List<Item> getNews(int quantity, int channelId) throws Exception;
 
-    List<Item> getNews(int quantity, int channelId, Date date) throws SQLException;
+    List<Item> getNews(int quantity, int channelId, Date date) throws Exception;
 
 
-    List<Item> getNewsInDay(int channelId, Date startDate) throws SQLException;
+    List<Item> getNewsInDay(int channelId, Date startDate) throws Exception;
 
-    List<Item> searchInContext(String phrase) throws SQLException;
+    List<Item> searchInContext(String phrase) throws Exception;
 
-    List<Item> searchInTitle(String phrase) throws SQLException;
+    List<Item> searchInTitle(String phrase) throws Exception;
 
-    List<String> getLatestNewsOfAgencies() throws SQLException;
+    List<String> getLatestNewsOfAgencies() throws Exception;
 
 }
